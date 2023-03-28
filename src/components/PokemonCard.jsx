f4df44adccbd0af7cbbde20226d5f7b19cf6f384
AHMED EL AMRI <ahmed-elamri@outlook.fr>
2023-03-28 12:54:41 +0200

    React Basics 03 - Pokédex - Afficher des données dans un composant FINI

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,27 @@
-
-
-
-
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function PokemonCard() {
-    return <figure>
-        < img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-            alt="pokemon 1" />
-        <figcaption> "bulbasaur" </figcaption>
-    </figure>;
+  const pokemon = pokemonList[0];
+
+  return (
+    <figure>
+      {pokemon.imgSrc != null ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>??? </p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
 }
 
 export default PokemonCard;
