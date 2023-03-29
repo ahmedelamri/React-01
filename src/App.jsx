c36c0e0490c63972aec996360a52d845da83094c
AHMED EL AMRI <ahmed-elamri@outlook.fr>
2023-03-29 10:59:41 +0200

    React Basics 04  Fini

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import PokemonCard from "./components/PokemonCard";
 
-import PokemonCard from "./components/PokemonCard"
+function App(props) {
+  console.log(props);
 
-function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
     </div>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default App;
